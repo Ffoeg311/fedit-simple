cc7cebb5b0511edfe4b1e538b00c30343f71b9d1
GH Porter <geoff.porter@gmail.com>
2014-08-19 21:33:19 -0400

    Added a file dialog for opening files.

--- fedit.cpp
+++ fedit.cpp
@@ -1,14 +1,62 @@
 #include "fedit.h"
 #include "ui_fedit.h"
+#include <QFileDialog>
+#include <iostream> //for testing
 
 Fedit::Fedit(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::Fedit)
 {
     ui->setupUi(this);
+
 }
 
 Fedit::~Fedit()
 {
     delete ui;
 }
+
+/*Methods for the textEditorPage buttons*/
+
+void Fedit::on_openButtonEditor_clicked()
+{
+    std::cout << "hello open button" << std::endl; //SC
+    switchToOpenWidget();
+}
+
+void Fedit::on_saveButtonEditor_clicked()
+{
+    std::cout << "hello save button" << std::endl; //SC
+}
+
+void Fedit::on_searchButtonEdit_clicked()
+{
+    std::cout << "hello search button" << std::endl; //SC
+}
+
+/*Methods for openFilePage buttons*/
+
+void Fedit::on_cancelButton_ow_clicked()
+{
+    //std::cout << "hello cancel button" << std::endl; //SC
+    switchToEditorWidget();
+}
+
+void Fedit::on_openButton_ow_clicked()
+{
+    //std::cout << "hello open button" << std::endl; //SC
+}
+
+/*Helper methods*/
+
+void Fedit::switchToOpenWidget()
+{
+    QFileDialog::getOpenFileName(this,
+         tr("Open File"));
+}
+
+void Fedit::switchToEditorWidget(){
+    std::cout << "switching" << std::endl; //SC
+    ui->stackedWidget->setCurrentWidget(ui->textEditorPage);
+}
+
--- fedit.h
+++ fedit.h
@@ -15,8 +15,17 @@ public:
     explicit Fedit(QWidget *parent = 0);
     ~Fedit();
 
+private slots:
+    void on_openButtonEditor_clicked();
+    void on_saveButtonEditor_clicked();
+    void on_searchButtonEdit_clicked();
+    void on_cancelButton_ow_clicked();
+    void on_openButton_ow_clicked();
+
 private:
     Ui::Fedit *ui;
+    void switchToOpenWidget();
+    void switchToEditorWidget();
 };
 
 #endif // FEDIT_H
